88d81eb07df0b6d370a6fcc8e257da22c5e55ef1
mana-byte <manaikilaut@gmail.com>
2026-03-05 21:55:38 +0100

    package.json: add server to allowedHost

--- front_jeu/vite.config.js
+++ front_jeu/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['ec2-18-202-55-34.eu-west-1.compute.amazonaws.com']
+  }
 })
